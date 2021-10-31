83c1735d075f21a26381f69e713961b8ecc247d5
rob49157 <robertopaipo@hotmail.com>
2021-10-31 08:56:09 -0400

    updating files

--- engineer.js
+++ engineer.js
@@ -3,26 +3,36 @@ class Engineer extends employee{
     super(name,email)
     }
 
-    question= inquirer.prompt([{
-        type:'input',
-        message:' Whats your name?',
-        name: 'name'},
-  
-        {type:'input',
-        message:"Enter id:",
-        name:'id'},
-  
-        {type:'input',
-        message:'Enter email:',
-        name:'email:'},
-
-        {type:'input',
-        message:'Enter github:',
-        name:'github'}
-      ])
-
+    
 
     github(){
+        question= inquirer.prompt([{
+            type:'input',
+            message:' Whats your name?',
+            name: 'name'},
+      
+            {type:'input',
+            message:"Enter id:",
+            name:'id'},
+      
+            {type:'input',
+            message:'Enter email:',
+            name:'email:'},
+    
+            {type:'input',
+            message:'Enter github:',
+            name:'github'},
+
+
+          ]).then(function(response){
+            $template +=`<div id= engineer> Engineer name ${response.name} \n\n Engineer id: ${response.name}\n\n Engineer Email ${response.email}\n\n Engineer Github ${response.github}`
+            fs.writeFile(html,function(err){
+                if(err){
+                    console.log(err)
+                }
+            })
+      
+          })
 
     }
 
